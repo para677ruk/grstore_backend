117659311e21c795bb0e53a08091e186c837c42c
para677ruk <epicgamesfalloutnew@gmail.com>
2025-06-09 23:53:53 +0400

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(cors({
-  origin: ["localhost:5173"],
+  origin: ["https://grstore-frontend-1.onrender.com"],
   credentials: true,
   
 }));
