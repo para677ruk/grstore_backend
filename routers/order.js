5b48ed6d158146341f1eaf6aaba0309f32ff4fbe
para677ruk <epicgamesfalloutnew@gmail.com>
2025-06-10 00:52:15 +0400

    Update order.js

--- routers/order.js
+++ routers/order.js
@@ -5,11 +5,13 @@ const nodemailer = require('nodemailer');
 
 router.post('/', async (req, res) => {
   try {
+    console.log('Получен заказ:', req.body);
     const order = new Order(req.body);
     await order.save();
 
     res.json({ success: true, order });
   } catch (e) {
+    console.error('Ошибка при сохранении заказа:', e);
     res.status(500).json({ success: false, message: 'Ошибка при сохранении заказа' });
   }
 });
